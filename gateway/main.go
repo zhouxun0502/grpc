261a27f5990bb0c14887b4d13a45e9537ddb4ce5
zhouxun <zhouxun@zhuge.com>
2022-07-07 11:10:54 +0800

    add grpc-gateway

---
+++ gateway/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/felixge/httpsnoop"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/soheilhy/cmux"
+	// importing generated stubs
+	gen "grpc/gateway/gen/go/hello"
+
+	"google.golang.org/grpc"
+)
+
+// GreeterServerImpl will implement the service defined in protocol buffer definitions
+type GreeterServerImpl struct {
+	gen.UnimplementedGreeterServer
+}
+
+// SayHello is the implementation of RPC call defined in protocol definitions.
+// This will take HelloRequest message and return HelloReply
+func (g *GreeterServerImpl) SayHello(ctx context.Context, request *gen.HelloRequest) (*gen.HelloReply, error) {
+	if err := request.Validate(); err != nil {
+		return nil, err
+	}
+	return &gen.HelloReply{
+		Message: fmt.Sprintf("hello %s %s", request.Name, request.LastName),
+	}, nil
+}
+func main() {
+	// create new gRPC server
+	grpcSever := grpc.NewServer()
+	// register the GreeterServerImpl on the gRPC server
+	gen.RegisterGreeterServer(grpcSever, &GreeterServerImpl{})
+	// creating mux for gRPC gateway. This will multiplex or route request different gRPC service
+	mux := runtime.NewServeMux()
+	// setting up a dail up for gRPC service by specifying endpoint/target url
+	err := gen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, "localhost:8081", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Creating a normal HTTP server
+	server := http.Server{
+		Handler: withLogger(mux),
+	}
+	// creating a listener for server
+	l, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := cmux.New(l)
+	// a different listener for HTTP1
+	httpL := m.Match(cmux.HTTP1Fast())
+	// a different listener for HTTP2 since gRPC uses HTTP2
+	grpcL := m.Match(cmux.HTTP2())
+	// start server
+	// passing dummy listener
+	go server.Serve(httpL)
+	// passing dummy listener
+	go grpcSever.Serve(grpcL)
+	// actual listener
+	m.Serve()
+}
+func withLogger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		m := httpsnoop.CaptureMetrics(handler, writer, request)
+		log.Printf("http[%d]-- %s -- %s\n", m.Code, m.Duration, request.URL.Path)
+	})
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,13 @@ module grpc
 go 1.16
 
 require (
-	google.golang.org/grpc v1.44.0
-	google.golang.org/protobuf v1.27.1
+	github.com/envoyproxy/protoc-gen-validate v0.6.7
+	github.com/felixge/httpsnoop v1.0.3
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3
+	github.com/soheilhy/cmux v0.1.5
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
+	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd
+	google.golang.org/grpc v1.46.2
+	google.golang.org/protobuf v1.28.0
 )
